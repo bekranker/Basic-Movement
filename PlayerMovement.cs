998709dbf2e9bfb6a2c77c3a4a02371a9e4afffb
BEKİR <62219677+bekranker@users.noreply.github.com>
2022-01-07 04:58:36 +0300

    Update PlayerMovement.cs

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -7,8 +7,10 @@ public class PlayerMovement : MonoBehaviour
     public float speed; // it's your characters speed
     public float jumpAmount; // it's your character jump coefficient
     private Rigidbody2D rb; // this is our Rigidbody component
-    bool IsGrounded = false; // we're checking to touching ground for jump.
-
+    public Layer GroundLayer;
+    public Transform feetPos;
+    public float checkRadius;
+    
 
     private void Awake()
     {
@@ -18,42 +20,21 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         var X = Input.GetAxis("Horizontal") * Time.deltaTime; // it's getting Inputs from player.we're getting values and we're say them var X.
-        //var Y = Input.GetAxis("Vertical") * Time.deltaTime; you should add axis y for your project.
+        var Y = Input.GetAxis("Vertical") * Time.deltaTime; //you should add y axis for your project.
 
         transform.position += new Vector3(X * speed, 0, 0); //this is chancing position for your character every second.
-
+        IsGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, GroundLayer); // thats for jumping.
 
 
     }
 
     private void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded == true)
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded)
         {
             rb.AddForce(transform.up * jumpAmount, ForceMode2D.Impulse); // we're adding some force for our character. but just for for up. 
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        // are we touch the ground ?
-        if (collision.gameObject.name == "ground") //if answer's yes
-        {
-            IsGrounded = true; // change to true
-        }
 
-        
-    }
-
-    private void OnCollisionExit2D(Collision2D collisionE)
-    {
-        // are we break to touch the ground ?
-
-        if (collisionE.gameObject.name == "ground") // if answer's yes
-        {
-
-            IsGrounded = false; // change to false
-
-        }
-    }
 }
